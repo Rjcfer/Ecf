c8dc0dbdaa8bf90ee844ffae35c746807e342610
Ricardo <Correia.ferreira.ricardo@gmail.com>
2022-03-28 10:56:17 +0200

    bug fix

--- migrations/Version20220328083614.php
+++ migrations/Version20220328083614.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220328083714 extends AbstractMigration
+final class Version20220328083614 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,10 +20,6 @@ final class Version20220328083714 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE hotel');
-        $this->addSql('DROP TABLE reservation');
-        $this->addSql('DROP TABLE suite');
-        $this->addSql('DROP TABLE user');
         $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, address VARCHAR(255) NOT NULL, name VARCHAR(50) NOT NULL, picture_url VARCHAR(255) DEFAULT NULL, city VARCHAR(100) NOT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reservation (id INT AUTO_INCREMENT NOT NULL, suite_id INT DEFAULT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, user_id INT DEFAULT NULL, INDEX IDX_42C849554FFCB518 (suite_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE suite (id INT AUTO_INCREMENT NOT NULL, hotel_id INT NOT NULL, name VARCHAR(50) NOT NULL, occupied TINYINT(1) NOT NULL, occupied_by VARCHAR(100) DEFAULT NULL, price DOUBLE PRECISION NOT NULL, pictures_url LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', title VARCHAR(255) DEFAULT NULL, booking_url VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, main_picture_url VARCHAR(255) NOT NULL, INDEX IDX_153CE4263243BB18 (hotel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
